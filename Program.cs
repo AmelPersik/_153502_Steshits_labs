c814659dfcaff1ece122e64788b65b9f5679de2a
stdas <stdas@LAPTOP-G8IKIBKL>
2022-03-10 12:57:31 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+class Program {
+    static void Main(string[] args) {
+        double a, b, x;
+        Console.Write("Input the first number: ");
+        a = Convert.ToDouble(Console.ReadLine());
+        Console.Write("Enter the second number: ");
+        b = Convert.ToDouble(Console.ReadLine());
+        x = a / b;
+        Console.WriteLine(x);
+    }
+}
